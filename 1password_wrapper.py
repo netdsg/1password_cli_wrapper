ebe4f6c62eb525ddb31a8c783db008b0a881e24e
netdsg <netdsg@naver.com>
2020-04-05 07:36:41 +0900

    session timeout improvments

--- 1password_wrapper.py
+++ 1password_wrapper.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+### environment variables 
+
 eHash = {
     'initPrompt': '\$', ### the prompt your shell uses.  
     'opTool': './op', ### path to the 1password cli tool binary
@@ -10,81 +12,74 @@ eHash = {
 
 badHabits = 'bad_habits.json'
 
+timeOut = 20 
+
+##################################
+
 import json, getpass, subprocess, pexpect, os, time, re, sys
+#from pprint import pprint
 
-if os.path.exists(badHabits):
-    try:
-        with open(badHabits) as ourJson:
-            eHash = json.load(ourJson)
-    except:
-        pass
-else:
-    if eHash['url'] == 'NULL':
-        eHash['url'] = input('Enter your sign-in address (url): ')
-    if eHash['email'] == 'NULL':
-        eHash['email'] = input('Enter your email address: ')
-    if eHash['key'] == 'NULL':
-        eHash['key'] = getpass.getpass(prompt='Enter your Secret Key: ')
-    eHash['ourPass'] = getpass.getpass(prompt='Enter your Master Password: ')
-
-def logoutOption():
+def logoutOption(S, ourPrompt):
     option = input('press enter to continue or q to quit... ')
     if option == 'q':
-        signOut()
+        signOut(S, ourPrompt)
         return(1)
     else:
         return(0)
 
-def signOut():
+def signOut(S, ourPrompt):
     S.sendline(eHash['opTool'] + ' signout')
     time.sleep(2)
     S.expect(ourPrompt)
     #print(S.before.decode())
     #print(S.after.decode())
 
-def logIn():
-    print('Signing into 1password...')
-    S = pexpect.spawn('bash')
-    S.expect(eHash['initPrompt'])
-    ourPrompt = S.before.decode().split()[0]
+def userLoop(deRail):
+    if deRail == 1:
+        ### login routine
+        print('Signing into 1password...')
+        S = pexpect.spawn('bash')
+        S.expect(eHash['initPrompt'])
+        ourPrompt = S.before.decode().split()[0]
 
-    S.sendline(eHash['opTool'] + ' signin ' + eHash['url'] + ' ' + eHash['email'] + ' ' + eHash['key'])
-    S.expect('at ' + eHash['url'].split('//')[1] + ':')
+        S.sendline(eHash['opTool'] + ' signin ' + eHash['url'] + ' ' + eHash['email'] + ' ' + eHash['key'])
+        S.expect('at ' + eHash['url'].split('//')[1] + ':')
 
-    S.sendline(eHash['ourPass'])
-    S.expect('session token')
+        S.sendline(eHash['ourPass'])
+        S.expect('session token')
 
-    tokenText = S.before.decode()
+        tokenText = S.before.decode()
 
-    tokenList = tokenText.splitlines()
-    for l in tokenList:
-        if 'OP_SESSION' in l:
-            sessionToken = l
+        tokenList = tokenText.splitlines()
+        for l in tokenList:
+            if 'OP_SESSION' in l:
+                sessionToken = l
 
-    S.sendline(sessionToken)
-    S.expect('\$')
+        S.sendline(sessionToken)
+        S.expect('\$')
 
-    print('Requesting list of items...')
-    S.sendline(eHash['opTool'] + ' list items')
-    time.sleep(1)
-    S.expect(ourPrompt)
-    while True:
-        if 'OP_SESSION' in S.before.decode():
-            S.sendline('\n')
-            S.expect(ourPrompt)
-            print('waiting for 1password items...')
-            time.sleep(.5)
-        else:
-            break
+        print('Requesting list of items...')
+        S.sendline(eHash['opTool'] + ' list items')
+        time.sleep(1)
+        S.expect(ourPrompt)
+        while True:
+            if 'OP_SESSION' in S.before.decode():
+                S.sendline('\n')
+                S.expect(ourPrompt)
+                print('waiting for 1password items...')
+                time.sleep(.5)
+            else:
+                break
 
-    rawItems = S.before.decode().rstrip()
-    rawSearch = re.search(r'.+op list items(\r\n)+(.+)', rawItems, re.DOTALL)
-    itemList = json.loads(rawSearch.group(2), strict=False)
-    return itemList, ourPrompt, S
-
-def userLoop():
-    while True:
+        rawItems = S.before.decode().rstrip()
+        rawSearch = re.search(r'.+op list items(\r\n)+(.+)', rawItems, re.DOTALL)
+        itemList = json.loads(rawSearch.group(2), strict=False)
+        #pprint(itemList)
+        
         deRail = 0
+########################################
+    ### main routine
+    while True:
         searchHash = {}
         searchTerm = input('\nEnter search term: ')
         ourCounter = 0
@@ -107,7 +102,7 @@ def userLoop():
                     print(sys.exc_info())
                     pass
             if accountSelect == 'q':
-                signOut()
+                signOut(S, ourPrompt)
                 #break
                 sys.exit()
             if accountSelect != 's':
@@ -119,10 +114,10 @@ def userLoop():
                 ourWait = 0
                 while True:
                     ourWait += 1
-                    if ourWait >= 20:
+                    if ourWait >= timeOut:
                         print('Session timeout... restarting routine...')
-                        signOut()
-                        logIn()
+                        signOut(S, ourPrompt)
+                        S.close()
                         deRail = 1
                         break
                     elif 'uuid' not in S.before.decode():
@@ -137,6 +132,7 @@ def userLoop():
                 details = S.before.decode().rstrip()
                 rawSearch = re.search(r'.+op.+(\r\n)+(.+)', details, re.DOTALL)
                 detailsHash = json.loads(rawSearch.group(2), strict=False)
+                #pprint(detailsHash)
                 print()
                 ### usual case
                 if 'fields' in detailsHash['details']:
@@ -171,19 +167,31 @@ def userLoop():
                                                 S.sendline(eHash['opTool'] + ' get totp ' + searchHash[int(accountSelect)]['uuid'])
                                                 time.sleep(1)
                                                 S.expect(ourPrompt)
+                                                ourWait = 0
                                                 while True:
-                                                    if 'op get totp' not in S.before.decode():
+                                                    ourWait += 1
+                                                    if ourWait >= timeOut:
+                                                        print('Session timeout... restarting routine...')
+                                                        signOut(S, ourPrompt)
+                                                        S.close()
+                                                        deRail = 1
+                                                        break
+                                                    elif 'op get totp' not in S.before.decode():
                                                         print('waiting for otp...')
                                                         S.sendline('\n')
                                                         time.sleep(.5)
                                                         S.expect(ourPrompt)
                                                     else:
                                                         break
+                                                if deRail == 1:
+                                                    break
                                                 otpReturn = S.before.decode().rstrip()
                                                 otpSearch = re.match(r'.+op get totp.+(\r\n)+(.+)', otpReturn)
                                                 
                                                 print('\notp:  ' + otpSearch.group(2))
-                ourOption = logoutOption()
+                if deRail == 1:
+                    break
+                ourOption = logoutOption(S, ourPrompt)
                 if ourOption == 1:
                     #break
                     sys.exit()
@@ -192,6 +200,21 @@ def userLoop():
 ### This is run time
 #################################################
 
-itemList, ourPrompt, S = logIn()
+if os.path.exists(badHabits):
+    try:
+        with open(badHabits) as ourJson:
+            eHash = json.load(ourJson)
+    except:
+        pass
+else:
+    if eHash['url'] == 'NULL':
+        eHash['url'] = input('Enter your sign-in address (url): ')
+    if eHash['email'] == 'NULL':
+        eHash['email'] = input('Enter your email address: ')
+    if eHash['key'] == 'NULL':
+        eHash['key'] = getpass.getpass(prompt='Enter your Secret Key: ')
+    eHash['ourPass'] = getpass.getpass(prompt='Enter your Master Password: ')
+
+
 while True:
-    userLoop()
+    userLoop(1)
